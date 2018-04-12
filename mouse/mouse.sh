c82c46b89cd221e664ba3d6a11c785cbb639d6a7
Manuel Turetta <manuel.turetta94@gmail.com>
2018-04-12 22:14:46 +0200

    Improvements - Parse mouse id from xinput list command output.

--- mouse/mouse.sh
+++ mouse/mouse.sh
@@ -3,6 +3,5 @@
 sleep 3;
 export DISPLAY=":0.0"
 export XAUTHORITY="/home/manu/.Xauthority"
-/usr/bin/xinput set-prop 12 'Coordinate Transformation Matrix' 2.0 0.0 0.0 0.0 2.0 0.0 0.0 0.0 1.0
-
-
+MOUSE_ID=$(/usr/bin/xinput list | grep "Logitech MX Master" | cut -d '=' -f 2 | cut -d '[' -f 1 | sed -n 1p)
+/usr/bin/xinput set-prop $MOUSE_ID 'Coordinate Transformation Matrix' 2.0 0.0 0.0 0.0 2.0 0.0 0.0 0.0 1.0
