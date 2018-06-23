5011fba7b24f2d196c0d2f3ce10f9e71aefba5cf
Manu@ares-xps15 <manuel.turetta94@gmail.com>
2018-06-23 16:48:45 +0200

    Polybar - Removed some unused modules; - Start changing icon style to material.

--- polybar/install.sh
+++ polybar/install.sh
@@ -9,7 +9,7 @@ echo "Coping files..."
 cp -r $DIR $HOME/.config/polybar
 
 DEPENDENCIES=('noto-fonts' 'ttf-roboto' 'ttf-font-awesome' 'zvbi')
-AUR_DEPENDENCIES=('ttf-unifont' 'siji-git' 'polybar')
+AUR_DEPENDENCIES=('ttf-unifont' 'ttf-material-icons' 'polybar')
 install_dependencies ${DEPENDENCIES[@]} ${AUR_DEPENDENCIES[@]}
 
 # ip link show | grep BROADCAST | cut -d":" -f2 | tr -d ' '
