597e7f21590d568df64ddc6eafae3c8e122264d8
Manu@ares-xps15 <manuel.turetta94@gmail.com>
2018-11-08 09:58:38 +0100

    Added "dunst" as notifications daemon

--- i3/install.sh
+++ i3/install.sh
@@ -8,8 +8,11 @@ safe_mkdir $HOME/.i3
 echo "Coping files..."
 cp -a $DIR/. $HOME/.i3/
 
-DEPENDENCIES=('network-manager-applet' 'nitrogen' 'pavucontrol')
+DEPENDENCIES=('network-manager-applet' 'nitrogen' 'pavucontrol', 'dunst')
 AUR_DEPENDENCIES=('python-pywal' 'volctl' 'twmn-git')
 install_dependencies ${DEPENDENCIES[@]} ${AUR_DEPENDENCIES[@]}
 
 wal -i $HOME/.i3/wallpaper.jpg -n
+# Seting up dunst
+mkdir $HOME/.config/dunst
+cp /usr/share/dunst/dunstrc $HOME/.config/dunst/.
