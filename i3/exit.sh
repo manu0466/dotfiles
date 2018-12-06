fca718e591e3fbd4572850a40ddddf7cd0b2a7b3
Manu@ares-xps15 <manuel.turetta94@gmail.com>
2018-12-06 11:04:10 +0100

    I3 Use dmenu to prompt the logout message

---
+++ i3/exit.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+while [ "$select" != "Yes" -a "$select" != "No" ];
+do
+	select=$(echo -e 'Yes\nNo' | dmenu  -nb '#0c2f35' -sf '#ffffff' -sb '#34AACB' -nf '#a6d7de' -i -p "Do you really want to exit i3?")
+	[ -z "$select" ] && exit 0
+done
+[ "$select" = "No" ] && exit 0
+[ "$select" = "Yes" ] && i3-msg exit
+
