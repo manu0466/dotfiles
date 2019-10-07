57e021a3b71cb5e37a8c2ab06fe29ad393f48b48
Manuel <manuel.turetta94@gmail.com>
2019-10-07 15:39:18 +0200

    Work configurations

--- i3/exit.sh
+++ i3/exit.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 while [ "$select" != "Yes" -a "$select" != "No" ];
 do
-	select=$(echo -e 'Yes\nNo' | dmenu  -nb '#0c2f35' -sf '#ffffff' -sb '#34AACB' -nf '#a6d7de' -i -p "Do you really want to exit i3?")
+	select=$(echo 'Yes\nNo' | dmenu  -nb '#0c2f35' -sf '#ffffff' -sb '#34AACB' -nf '#a6d7de' -i -p "Do you really want to exit i3?")
 	[ -z "$select" ] && exit 0
 done
 [ "$select" = "No" ] && exit 0
