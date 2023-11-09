48d0aec811a2d6aa5d5150648f21897305152986
Manuel <manuel.turetta94@gmail.com>
2023-11-09 15:20:11 +0100

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env sh
-# set -x #echo on
-echo "=== CONFIGURING I3 ==="
-./i3/install.sh
-echo "=== I3 DONE ==="
-echo
 
-echo "=== CONFIGURING POLYBAR ==="
-./polybar/install.sh
-echo "=== POLYBAR DONE ==="
-echo
+# Get the current directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Install alacritty configurations
+rm -rf $HOME/.config/alacritty
+ln -s $DIR/alacritty $HOME/.config/alacritty
+
+# Install i3 configurations
+rm -rf $HOME/.config/i3
+ln -s $DIR/i3 $HOME/.config/i3
+
+# Install picom configurations
+rm -rf $HOME/.config/picom
+ln -s $DIR/picom $HOME/.config/picom
+
+# Install polybar configurations
+rm -rf $HOME/.config/polybar
+ln -s $DIR/polybar $HOME/.config/polybar
+
+# Install rofi configurations
+rm -rf $HOME/.config/rofi
+ln -s $DIR/rofi $HOME/.config/rofi
+
+# Install tmux
+rm -rf $HOME/.config/tmux
+ln -s $DIR/tmux $HOME/.config/tmux
