dc15c98f0703a72b8077f8d3e9e246ab27a8b121
Manuel <manuel.turetta94@gmail.com>
2023-12-18 14:39:35 +0100

    feat: allow to install host specific configs

--- install.sh
+++ install.sh
@@ -4,11 +4,24 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 install() {
-    echo "Installing $1"
+    # Generic config path
+    config_path=$DIR/$1
     
-    # Install dotfiles
-    rm -rf $HOME/.config/$1
-    ln -s $DIR/$1 $HOME/.config/$1
+    # Get the host specific config path
+    hostname=$(cat /etc/hostname)
+    host_config_path=$DIR/$1-$hostname
+    
+    if [ -d $host_config_path ]; then
+        echo "Installing $1 for $hostname"
+        rm -rf $HOME/.config/$1
+        ln -s $host_config_path $HOME/.config/$1
+    elif [ -d $config_path ]; then
+        echo "Installing $1"
+        rm -rf $HOME/.config/$1
+        ln -s $config_path $HOME/.config/$1
+    else
+        echo "Can't find $1 config dir"
+    fi
 }
 
 install alacritty
