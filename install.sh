63bd7b512ebfa5ea913f4b0ca1a8f57c69f8acb9
Manuel <manuel.turetta94@gmail.com>
2023-12-18 14:27:28 +0100

    feat: new install script

--- install.sh
+++ install.sh
@@ -3,26 +3,17 @@
 # Get the current directory of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Install alacritty configurations
-rm -rf $HOME/.config/alacritty
-ln -s $DIR/alacritty $HOME/.config/alacritty
+install() {
+    echo "Installing $1"
+    
+    # Install dotfiles
+    rm -rf $HOME/.config/$1
+    ln -s $DIR/$1 $HOME/.config/$1
+}
 
-# Install i3 configurations
-rm -rf $HOME/.config/i3
-ln -s $DIR/i3 $HOME/.config/i3
-
-# Install picom configurations
-rm -rf $HOME/.config/picom
-ln -s $DIR/picom $HOME/.config/picom
-
-# Install polybar configurations
-rm -rf $HOME/.config/polybar
-ln -s $DIR/polybar $HOME/.config/polybar
-
-# Install rofi configurations
-rm -rf $HOME/.config/rofi
-ln -s $DIR/rofi $HOME/.config/rofi
-
-# Install tmux
-rm -rf $HOME/.config/tmux
-ln -s $DIR/tmux $HOME/.config/tmux
+install alacritty
+install i3
+install picom
+install polybar
+install rofi
+install tmux
